e29b5180fe468a56fe1ba849ee5b561b35511256
Alexander547 <guguckins72@gmail.com>
2023-04-16 23:02:13 +0300

    12

--- main.py
+++ main.py
@@ -10,47 +10,38 @@ root.config(bg='#B186B9')
 
 
 # Logic
-def addbd(t):
-      files = open('bd.txt', "a+")
-      files.write(t + '\n')
-      files.close()
+tasks = []
 
-def readbd():
-    files1 = open('bd.txt', "r+")
-    res = files1.readlines()
-    files1.close()
-    return res
-
-def delete(d1):
-    res12=readbd()
-    counter = 0
-    s=open("bd.txt", "w+")
-    for i in range(len(res12)):
-        if i==d1:
-            res12.pop(counter)
-            print(counter)
-        counter+=1
-        print(counter)
-    s.writelines(res12)
-    s.close()
-
-listing = readbd()
-listing_var = StringVar(value=listing) 
 
 def addtask():
     task = en.get()
-    if task !="":
-        addbd(task)    
+    if task !="":  
         listbox.insert(END, task) # добавление задач
+        tasks.append(task)
         en.delete(0, END)  # очистка инпута
+        save_task()
         
-
 def deltask():
     select = listbox.curselection()
     if select:
-        get_dels = listbox.get(select)
+        task_index = int(select[0])
+        del tasks[task_index]
         listbox.delete(select) # удаление задач из списка
-        delete(get_dels) # удаление задач из базы данных
+        save_task()
+
+def save_task():
+    f=open("bd.txt", "w")
+    for t in tasks:
+        f.write(f"{t}\n")
+
+def load_tasks():
+        f1 = open("bd.txt","r")
+        tasks1 = f1.readlines()
+        for t1 in tasks1:
+             t1 = t1.strip()
+             if t1:
+                  tasks.append(t1)
+                  listbox.insert(END,t1) 
 
 
 # Content
@@ -60,7 +51,7 @@ Title = Label(root,
                   font=('Arial',20, 'bold'),
                   foreground='white',
                   )
-listbox = Listbox(root, height=20, width=34, listvariable=(listing_var))
+listbox = Listbox(root, height=20, width=34, ) # listvariable=(listing_var)
 en = Entry(root)
 addButton = Button(root, text='Добавить', justify='center', command= addtask)
 delButton = Button(root, text='Удалить',  justify='center', command= deltask)
@@ -72,7 +63,7 @@ listbox.grid(row=1, column=0)
 en.grid(row=2, column=0, ipadx=40, ipady=6, padx=5, pady=5, columnspan=2)
 addButton.grid(row=3, column=0, columnspan=2, ipadx=70, ipady=6, padx=5, pady=5)
 delButton.grid(row=4, column=0, columnspan=2, ipadx=74, ipady=6, padx=5, pady=5)
-
+load_tasks()
 
 # Run
 root.mainloop()
